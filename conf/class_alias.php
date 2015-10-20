9655c1a275b07bfa77c8cdffc0c951ccf1382485
HarveyZ <geekwebme@gmail.com>
2015-10-21 01:59:27 +0800

    psr-0支持

---
+++ conf/class_alias.php
@@ -0,0 +1,7 @@
+<?php 
+return [
+	'HZF_Loader'      => 'HZF\Loader\Loader',
+	'HZF_Config'      => 'HZF\Config\Config',
+	'HZF_Router'      => 'HZF\Route\Router',
+	'HZF_Intercepter' => 'HZF\Route\Intercepter',
+];
