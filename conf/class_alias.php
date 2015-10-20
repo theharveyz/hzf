1ca3d5e417bf6969391d95243c175708ef47c372
HarveyZ <geekwebme@gmail.com>
2015-10-21 02:00:26 +0800

    Revert "psr-0支持"
    This reverts commit 9655c1a275b07bfa77c8cdffc0c951ccf1382485.

--- conf/class_alias.php
+++
@@ -1,7 +0,0 @@
-<?php 
-return [
-	'HZF_Loader'      => 'HZF\Loader\Loader',
-	'HZF_Config'      => 'HZF\Config\Config',
-	'HZF_Router'      => 'HZF\Route\Router',
-	'HZF_Intercepter' => 'HZF\Route\Intercepter',
-];
